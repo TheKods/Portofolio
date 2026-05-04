a65f5e4b14c0b38874f9fa9179bc7c0cddeba5e0
TheKods <rafihermawan06@gmail.com>
2026-05-04 14:31:29 +0700

    V4.1

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,7 @@ import Portofolio from "./Pages/Portofolio";
 import ContactPage from "./Pages/Contact";
 import NotFoundPage from "./Pages/404";
 import WelcomeOverlay from "./components/WelcomeOverlay";
+import LetterGlitch from "./components/LetterGlitch";
 import SoundCloudPlayer from "./components/SoundCloudPlayer";
 import SkillsSection from "./components/SkillsSection";
 import ExperienceSection from "./components/ExperienceSection";
@@ -28,25 +29,35 @@ const LandingPage = () => {
 
       {!showWelcome && (
         <>
-          <SoundCloudPlayer
-            defaultVolume={0.8}
-            autoPlay
-            playlist={[
-              "https://soundcloud.com/user-356546060/memories-of-spring",
-              "https://soundcloud.com/user-356546060/colorful-flowers",
-              "https://soundcloud.com/user-356546060/echoes-in-blue",
-              "https://soundcloud.com/user-356546060/way-home",
-            ]}
-          />
-          <div className="relative z-10">
-            <Navbar />
-            <Home />
-            <About />
-            <ExperienceSection />
-            <SkillsSection />
-            <Portofolio />
-            <ContactPage />
-            <Footer />
+          <div className="relative min-h-screen">
+            <div className="absolute inset-0 -z-10">
+              <LetterGlitch
+                glitchSpeed={50}
+                centerVignette={true}
+                outerVignette={false}
+                smooth={true}
+              />
+            </div>
+            <SoundCloudPlayer
+              defaultVolume={0.8}
+              autoPlay
+              playlist={[
+                "https://soundcloud.com/user-356546060/memories-of-spring",
+                "https://soundcloud.com/user-356546060/colorful-flowers",
+                "https://soundcloud.com/user-356546060/echoes-in-blue",
+                "https://soundcloud.com/user-356546060/way-home",
+              ]}
+            />
+            <div className="relative z-10">
+              <Navbar />
+              <Home />
+              <About />
+              <ExperienceSection />
+              <SkillsSection />
+              <Portofolio />
+              <ContactPage />
+              <Footer />
+            </div>
           </div>
         </>
       )}
--- src/components/WelcomeOverlay.jsx
+++ src/components/WelcomeOverlay.jsx
@@ -1,5 +1,4 @@
-import React, { useEffect, useMemo } from "react";
-import LetterGlitch from "./LetterGlitch";
+import React, { useEffect } from "react";
 import { AnimatePresence, motion } from "framer-motion";
 
 export default function WelcomeOverlay({
@@ -15,47 +14,6 @@ export default function WelcomeOverlay({
     return () => clearTimeout(timer);
   }, [isOpen, durationMs, onClose]);
 
-  const options = useMemo(
-    () => ({
-      onSpeedUp: () => {},
-      onSlowDown: () => {},
-      distortion: "turbulentDistortion",
-      length: 400,
-      roadWidth: 10,
-      islandWidth: 2,
-      lanesPerRoad: 3,
-      fov: 90,
-      fovSpeedUp: 150,
-      speedUp: 2,
-      carLightsFade: 0.4,
-      totalSideLightSticks: 20,
-      lightPairsPerRoadWay: 40,
-      shoulderLinesWidthPercentage: 0.05,
-      brokenLinesWidthPercentage: 0.1,
-      brokenLinesLengthPercentage: 0.5,
-      lightStickWidth: [0.12, 0.5],
-      lightStickHeight: [1.3, 1.7],
-      movingAwaySpeed: [60, 80],
-      movingCloserSpeed: [-120, -160],
-      carLightsLength: [400 * 0.03, 400 * 0.2],
-      carLightsRadius: [0.05, 0.14],
-      carWidthPercentage: [0.3, 0.5],
-      carShiftX: [-0.8, 0.8],
-      carFloorSeparation: [0, 5],
-      colors: {
-        roadColor: 0x080808,
-        islandColor: 0x0a0a0a,
-        background: 0x000000,
-        shoulderLines: 0xffffff,
-        brokenLines: 0xffffff,
-        leftCars: [0xd856bf, 0x6750a2, 0xc247ac],
-        rightCars: [0x03b3c3, 0x0e5ea5, 0x324555],
-        sticks: 0x03b3c3,
-      },
-    }),
-    [],
-  );
-
   return (
     <AnimatePresence>
       {isOpen && (
@@ -66,15 +24,6 @@ export default function WelcomeOverlay({
           exit={{ opacity: 0 }}
           transition={{ duration: 0.4, ease: "easeInOut" }}
         >
-          {/* LetterGlitch background */}
-          <div className="absolute inset-0">
-            <LetterGlitch
-              glitchSpeed={50}
-              centerVignette={true}
-              outerVignette={false}
-              smooth={true}
-            />
-          </div>
           {/* Dim overlay */}
           <motion.div
             className="absolute inset-0 bg-black"
