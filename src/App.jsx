2b28f80e45ca125b19ecacd7e35492dfdabb5b2a
TheKods <rafihermawan06@gmail.com>
2026-05-04 14:35:08 +0700

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,7 @@ import Portofolio from "./Pages/Portofolio";
 import ContactPage from "./Pages/Contact";
 import NotFoundPage from "./Pages/404";
 import WelcomeOverlay from "./components/WelcomeOverlay";
+import Hyperspeed from "./components/Hyperspeed";
 import LetterGlitch from "./components/LetterGlitch";
 import SoundCloudPlayer from "./components/SoundCloudPlayer";
 import SkillsSection from "./components/SkillsSection";
@@ -27,39 +28,52 @@ const LandingPage = () => {
         durationMs={3000}
       />
 
-      {!showWelcome && (
-        <>
-          <div className="relative min-h-screen">
-            <div className="absolute inset-0 -z-10">
-              <LetterGlitch
-                glitchSpeed={50}
-                centerVignette={true}
-                outerVignette={false}
-                smooth={true}
-              />
-            </div>
-            <SoundCloudPlayer
-              defaultVolume={0.8}
-              autoPlay
-              playlist={[
-                "https://soundcloud.com/user-356546060/memories-of-spring",
-                "https://soundcloud.com/user-356546060/colorful-flowers",
-                "https://soundcloud.com/user-356546060/echoes-in-blue",
-                "https://soundcloud.com/user-356546060/way-home",
-              ]}
-            />
-            <div className="relative z-10">
-              <Navbar />
-              <Home />
-              <About />
-              <ExperienceSection />
-              <SkillsSection />
-              <Portofolio />
-              <ContactPage />
-              <Footer />
-            </div>
+      {showWelcome && (
+        <div className="relative min-h-screen">
+          <div className="absolute inset-0 -z-10">
+            <Hyperspeed />
           </div>
-        </>
+          <WelcomeOverlay
+            isOpen={showWelcome}
+            onClose={() => setShowWelcome(false)}
+            title="Welcome"
+            subtitle="Preparing your personalized portfolio..."
+            durationMs={3000}
+          />
+        </div>
+      )}
+
+      {!showWelcome && (
+        <div className="relative min-h-screen">
+          <div className="absolute inset-0 -z-10">
+            <LetterGlitch
+              glitchSpeed={50}
+              centerVignette={true}
+              outerVignette={false}
+              smooth={true}
+            />
+          </div>
+          <SoundCloudPlayer
+            defaultVolume={0.8}
+            autoPlay
+            playlist={[
+              "https://soundcloud.com/user-356546060/memories-of-spring",
+              "https://soundcloud.com/user-356546060/colorful-flowers",
+              "https://soundcloud.com/user-356546060/echoes-in-blue",
+              "https://soundcloud.com/user-356546060/way-home",
+            ]}
+          />
+          <div className="relative z-10">
+            <Navbar />
+            <Home />
+            <About />
+            <ExperienceSection />
+            <SkillsSection />
+            <Portofolio />
+            <ContactPage />
+            <Footer />
+          </div>
+        </div>
       )}
     </>
   );
